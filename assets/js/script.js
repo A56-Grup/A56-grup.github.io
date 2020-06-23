e2ee7b744330b4a7968f0f22d5bd3755a01165c5
fauzan264 <fauzan.c4@gmail.com>
2020-06-23 11:59:24 +0700

    tugas progate kelompok A56

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let menuButton = document.getElementsByClassName("menu-icon")[0];
+let menu = document.getElementsByClassName("header-right")[0];
+menuButton.addEventListener('click', function() {
+    menu.classList.toggle("show-menu");
+    // if(menu)
+});
